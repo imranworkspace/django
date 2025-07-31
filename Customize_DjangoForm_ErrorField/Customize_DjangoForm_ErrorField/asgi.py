54afd72ed8fd9bf2c50ee47947b7dffe57f1f897
imranlatur24 <imranlatur24@gmail.com>
2025-07-31 16:23:10 +0530

    Modelform,Customize django forms,fields and errors

---
+++ Customize_DjangoForm_ErrorField/Customize_DjangoForm_ErrorField/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Customize_DjangoForm_ErrorField project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Customize_DjangoForm_ErrorField.settings')
+
+application = get_asgi_application()
